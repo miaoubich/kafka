938f44c443c7ed9d84f636e01c0dccbaf95a8ae2
Ali Bouzar <ali.bouzar@gmail.com>
2024-03-20 16:04:05 -0600

    some changes

--- order-service/src/main/java/net/miaoubich/orderservice/controller/OrderController.java
+++ order-service/src/main/java/net/miaoubich/orderservice/controller/OrderController.java
@@ -1,10 +1,34 @@
 package net.miaoubich.orderservice.controller;
 
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+import net.miaoubich.basedomains.dto.Order;
+import net.miaoubich.basedomains.dto.OrderEvent;
+import net.miaoubich.orderservice.kafka.OrderProducer;
+
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/v1")
 public class OrderController {
 
+	private final OrderProducer orderProducer;
+	
+	@PostMapping("/order")
+	public ResponseEntity<String> placeOrder(@RequestBody Order order) {
+		order.setOrderId(UUID.randomUUID().toString());
+		OrderEvent orderEvent = new OrderEvent();
+		
+		orderEvent.setMessage("Order status is in pending state!");
+		orderEvent.setOrder(order);
+		orderProducer.sendMessage(orderEvent);
+		
+		return ResponseEntity.ok("Order placed successfully ...");
+	}
 }
